1d6726763ce1c39be9029c1ee85f11b9999a539a
Tom Davis <tom.davis@rumr.co.uk>
2021-04-21 22:28:29 +0100

    Extend Adafruit_LvGL_Glue with file system implementation.

---
+++ Adafruit_LvGL_Glue_SD.h
@@ -0,0 +1,28 @@
+#ifndef _ADAFRUIT_LVGL_GLUE_SD_H
+#define _ADAFRUIT_LVGL_GLUE_SD_H
+
+#include "Adafruit_LvGL_Glue.h"
+#include <SdFat.h>
+
+typedef File file_t;
+
+class Adafruit_LvGL_Glue_SD : public Adafruit_LvGL_Glue {
+public:
+    LvGLStatus begin(Adafruit_SPITFT *tft, Adafruit_STMPE610 *touch,
+                     SdFat *sdFat, bool debug = false);
+
+    LvGLStatus begin(Adafruit_SPITFT *tft, TouchScreen *touch,
+                     SdFat *sdFat, bool debug = false);
+
+    LvGLStatus begin(Adafruit_SPITFT *tft, SdFat *sdFat, bool debug = false);
+
+    // Needs to be public for internal callback
+    SdFat *sd;  ///< Pointer to SD card reader
+
+private:
+    void initFileSystem();
+    lv_fs_drv_t lv_fs_drv;
+};
+
+
+#endif //_ADAFRUIT_LVGL_GLUE_SD_H
